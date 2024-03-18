corrected scaling of early/late parts

src/cond_waveunet_loss.py
@@ -10,6 +10,7 @@ from distutils.version import LooseVersion
 import torch
 import torchaudio
 import torch.nn.functional as F
+import joa_helpers as hlp
 
 is_pytorch_17plus = LooseVersion(torch.__version__) >= LooseVersion("1.7")
 
@@ -200,7 +201,7 @@ class LossOfChoice(torch.nn.Module):
             self.criterion_audio=MultiResolutionSTFTLoss().to(device)
             self.criterion_emb=torch.nn.CosineSimilarity(dim=2,eps=1e-8).to(device)
 
-        elif self.losstype=="stft+rev+emb":
+        elif self.losstype=="stft+rev+emb" or self.losstype=="early+late+emb":
             self.criterion_audio=MultiResolutionSTFTLoss().to(device)
             self.criterion_emb=torch.nn.CosineSimilarity(dim=2,eps=1e-8).to(device)
         
@@ -217,8 +218,8 @@ class LossOfChoice(torch.nn.Module):
         sStyle_in=data[1].to(device) # s2r2 - style
         sTarget=data[2].to(device) # s1r2 - target
         sAnecho=data[3].to(device) # s1 - anechoic
-        sEarly=data[3].to(device) # s1r2_early - early reflections
-        sLate=data[3].to(device) # s1r2_late - late reverb
+        sEarly=data[4].to(device) # s1r2_early - early reflections
+        sLate=data[5].to(device) # s1r2_late - late reverb
         # sPositive=data[4].to(device) # s2r1
         # forward pass - get prediction of the ir
         embContent=model_reverbenc(sContent_in)
@@ -271,8 +272,8 @@ class LossOfChoice(torch.nn.Module):
         elif self.losstype=="early+late+emb":
             # get the embedding of the prediction
             embTarget=model_reverbenc(sTarget)
-            L_sc_early, L_mag_early = self.criterion_audio(sTarget.squeeze(1)-sLate.squeeze(1), sPrediction.squeeze(1)-sLate.squeeze(1))
-            L_sc_late, L_mag_late = self.criterion_audio(sTarget.squeeze(1)-sEarly.squeeze(1), sPrediction.squeeze(1)-sEarly.squeeze(1))
+            L_sc_early, L_mag_early = self.criterion_audio(hlp.torch_standardize_max_abs(sTarget.squeeze(1)-sLate.squeeze(1)), hlp.torch_standardize_max_abs(sPrediction.squeeze(1)-sLate.squeeze(1)))
+            L_sc_late, L_mag_late = self.criterion_audio(hlp.torch_standardize_max_abs(sTarget.squeeze(1)-sEarly.squeeze(1)), hlp.torch_standardize_max_abs(sPrediction.squeeze(1)-sEarly.squeeze(1)))
             L_early=L_sc_early+L_mag_early
             L_late=L_sc_late+L_mag_late
             L_emb=(1-((torch.mean(self.criterion_emb(embStyle,embTarget))+ 1) / 2))

src/cond_waveunet_trainer.py
@@ -71,8 +71,9 @@ class Trainer(torch.nn.Module):
             sStyle_in=data[1].to(self.args.device)
             sTarget_gt=data[2].to(self.args.device)
             # forward pass - get prediction of the ir
-            embedding_gt=self.model_reverbenc(sStyle_in)
-            sTarget_prediction=self.model_waveunet(sContent_in,embedding_gt)
+            embedding_enc=self.model_reverbenc(sContent_in)
+            embedding_dec=self.model_reverbenc(sStyle_in)
+            sTarget_prediction=self.model_waveunet(sContent_in,embedding_enc,embedding_dec)
             return sContent_in, sStyle_in, sTarget_gt, sTarget_prediction
         
     def logaudio_tboard(self,writer):

src/joa_helpers.py
@@ -44,11 +44,16 @@ def torch_standardize_std(sig_in):
     sig_out=(sig_in-mu)/sigma
     return sig_out
 
-def torch_standardize_max_abs(signal):
+def torch_standardize_max_abs(signal,out=False):
     max_abs_value = torch.max(torch.abs(signal))
     standardized_signal = signal / max_abs_value
-    standardized_signal -= torch.mean(standardized_signal)
-    return standardized_signal
+    # standardized_signal -= torch.mean(standardized_signal)
+    if out:
+        return standardized_signal, max_abs_value
+    else:
+        return standardized_signal
+
+
 
 def torch_resample_if_needed(audio,sr,sr_target):
     if sr!=sr_target:
@@ -236,9 +241,9 @@ def torch_deconv_W(reverberant_signal, room_impulse_response):
 
 
 def rir_split_earlylate(rir, fs, cutpoint_ms):
-    rir_early=np.zeros((rir.shape))
-    rir_late=np.zeros((rir.shape))
-    rir_early[:int(cutpoint_ms*fs)]=rir[:int(cutpoint_ms*fs)]
-    rir_late[int(cutpoint_ms*fs):]=rir[int(cutpoint_ms*fs):]
+    rir_early=torch.zeros_like(rir)
+    rir_late=torch.zeros_like(rir)
+    rir_early[:,:int(1e-3*cutpoint_ms*fs)]=rir[:,:int(1e-3*cutpoint_ms*fs)]
+    rir_late[:,int(1e-3*cutpoint_ms*fs):]=rir[:,int(1e-3*cutpoint_ms*fs):]
     return rir_early, rir_late
     